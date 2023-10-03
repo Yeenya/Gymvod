ecd5504e2ea8212f31c0f9cb703de98951b8a6fa
Jan Borecky <honza.borecky@seznam.cz>
2023-10-03 10:58:29 +0200

    Septimy week 6

---
+++ Septimy/6 - 11.10.2023/RecursionPlayground/RecursionPlayground/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Made by Jan Borecky for PRG seminar at Gymnazium Voderadska, year 2023-2024.
+ * Extended by students.
+ */
+
+namespace RecursionPlayground
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); // Nacteme cislo n, pro ktere budeme pocitat jeho faktorial a n-ty prvek Fibonacciho posloupnosti.
+            int factorial = Factorial(n); // Prvni zavolani pro vypocet faktorialu, ulozeni do promenne factorial.
+            int fibonacci = Fibonacci(n); // Prvni zavolani pro vypocet Fibonacciho posloupnosti, ulozeni do promenne fibonacci.
+            Console.WriteLine($"Pro cislo {n} je faktorial {factorial} a {n}. prvek Fibonacciho posloupnosti je {fibonacci}"); // Vypsani vysledku uzivateli.
+            Console.ReadKey();
+        }
+
+        static int Factorial(int n)
+        {
+            // TODO: Urci ukoncovaci podminku pro faktorial a zavolej Factorial zevnitr se spravnym parametrem / vypoctem.
+            return 0; // TODO: Uprav, aby Factorial vracel spravnou hodnotu.
+        }
+
+        static int Fibonacci(int n)
+        {
+            // TODO: Urci ukoncovaci podminku pro Fibonacciho a zavolej Fibonacci zevnitr se spravnym parametrem / vypoctem.
+            return 0; // TODO: Uprav, aby Fibonacci vracel spravnou hodnotu.
+        }
+    }
+}
