a48e5e24fefbe2ed0ea78c3d65c3c1bf8b9819b2
Jan Borecky <honza.borecky@seznam.cz>
2024-02-05 17:19:21 +0100

    Added Graph Playground

---
+++ Septimy/23 - 7.2.2024/GraphPlayground/GraphPlayground/Node.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace GraphPlayground
+{
+    //Simple node class, no comments needed
+    internal class Node
+    {
+        public int index;
+
+        public List<Node> neighbors;
+
+        public bool visited;
+
+        public Node(int index)
+        {
+            this.index = index;
+            neighbors = new List<Node>();
+            visited = false;
+        }
+    }
+}
---
+++ Septimy/23 - 7.2.2024/GraphPlayground/GraphPlayground/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GraphPlayground
+{
+    internal class Program
+    {
+        public static void DFS(Graph graph, Node startNode, Node targetNode = null)
+        {
+
+        }
+
+        public static void BFS(Graph graph, Node startNode, Node targetNode = null) 
+        {
+            
+        }
+
+        static void Main(string[] args)
+        {
+            //Create and print the graph
+            Graph graph = new Graph();
+            graph.PrintGraph();
+            graph.PrintGraphForVisualization();
+
+            //Call both algorithms with a random starting node
+            Random rng = new Random();
+            DFS(graph, graph.nodes[rng.Next(0, graph.nodes.Count)]);
+            BFS(graph, graph.nodes[rng.Next(0, graph.nodes.Count)]);
+
+            Console.ReadKey();
+        }
+    }
+}
