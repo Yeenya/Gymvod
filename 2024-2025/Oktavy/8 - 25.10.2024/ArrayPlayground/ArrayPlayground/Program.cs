cd35aa8feed29ac9e9b3597d75e97b80b7963a0a
Jan Borecky <honza.borecky@seznam.cz>
2024-10-24 13:08:52 +0200

    Week 8

---
+++ 2024-2025/Oktavy/8 - 25.10.2024/ArrayPlayground/ArrayPlayground/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Made by Jan Borecky for PRG seminar at Gymnazium Voderadska, year 2024-2025.
+ * Extended by students.
+ */
+
+namespace ArrayPlayground
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO 1: Vytvoř integerové pole a naplň ho pěti libovolnými čísly.
+
+
+            //TODO 2: Vypiš do konzole všechny prvky pole, zkus jak klasický for, kde i využiješ jako index v poli, tak foreach.
+
+
+            //TODO 3: Spočti sumu všech prvků v poli a vypiš ji uživateli.
+            int sum;
+
+
+            //TODO 4: Spočti průměr prvků v poli a vypiš ho do konzole.
+            int average;
+
+
+            //TODO 5: Najdi maximum v poli a vypiš ho do konzole.
+            int max;
+
+
+            //TODO 6: Najdi minimum v poli a vypiš ho do konzole.
+            int min;
+
+
+            //TODO 7: Vyhledej v poli číslo, které zadá uživatel, a vypiš index nalezeného prvku do konzole.
+            int index;
+
+            //TODO 8: Přepiš pole na úplně nové tak, že bude obsahovat 100 náhodně vygenerovaných čísel od 0 do 9.
+            Random rng = new Random();
+
+            //TODO 9: Spočítej kolikrát se každé číslo v poli vyskytuje a spočítané četnosti vypiš do konzole.
+            int[] counts = new int[10];
+
+            //TODO 10: Vytvoř druhé pole, do kterého zkopíruješ prvky z prvního pole v opačném pořadí.
+
+
+            //Zkus is dál hrát s polem dle své libosti. Můžeš třeba prohodit dva prvky, ukládat do pole prvky nějaké posloupnosti (a pak si je vyhledávat) nebo cokoliv dalšího tě napadne
+
+            Console.ReadKey();
+        }
+    }
+}
