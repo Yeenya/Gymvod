c46d6872bd346f7b106cf1f94aa483933da731f1
Jan Borecky <honza.borecky@seznam.cz>
2024-11-07 14:04:18 +0100

    Week 9

---
+++ 2024-2025/Oktavy/9 - 8.11.2024/Elections/Elections/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elections
+{
+    internal class Program
+    {
+        static void PrintCandidates(Dictionary<string, int> elections)
+        {
+            Console.WriteLine("Current state of the elections:");
+            if (elections.Count > 0)
+            {
+                foreach (KeyValuePair<string, int> electionSubject in elections)
+                {
+                    Console.WriteLine("    Candidate " + electionSubject.Key + " has " + electionSubject.Value + " votes.");
+                }
+            } else
+            {
+                Console.WriteLine("There are no candidates in the elections.");
+            }
+        }
+
+        static void EvaluateElections(Dictionary<string, int> elections)
+        {
+            if (elections.Count == 0)
+            {
+                Console.WriteLine("There are no candidates, there is nothing to evaluate.");
+                return;
+            }
+            int totalVotes = elections.Values.Sum();
+            string winner = elections.Where(electionSubject => electionSubject.Value == elections.Values.Max()).First().Key;
+            foreach (KeyValuePair<string, int> electionSubject in elections)
+            {
+                float percentVotes = (float)electionSubject.Value / totalVotes * 100;
+                Console.WriteLine("Candidate " + electionSubject.Key + " has " + electionSubject.Value + " votes, which make " + percentVotes + " % of the total votes.");
+            }
+            Console.WriteLine("Winner of the elections is candidate " + winner + ".");
+        }
+
+        static void AddCandidate(Dictionary<string, int> elections, string candidate)
+        {
+            if (!elections.Keys.Contains(candidate)) elections[candidate] = 0;
+            else Console.WriteLine("This candidate is already in the elections.");
+        }
+
+        static void RemoveCandidate(Dictionary<string, int> elections, string candidate)
+        {
+            if (elections.Keys.Contains(candidate)) elections.Remove(candidate);
+            else Console.WriteLine("This candidate is already not present in the elections.");
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+
+        static void EraseElections(Dictionary<string, int> elections)
+        {
+            if (elections.Keys.Count > 0) elections.Clear();
+            else Console.WriteLine("Elections are already without any candidate.");
+            
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+
+        static void SetVotes(Dictionary<string, int> elections, string candidate, uint votes)
+        {
+            Console.WriteLine("Setting votes for candidate " + candidate + " to " + votes + ".");
+            elections[candidate] = Convert.ToInt32(votes);
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+
+        static void MakeRandomElections(Dictionary<string, int> elections)
+        {
+            if (elections.Count > 1)
+            {
+                Random rng = new Random();
+                int votersCount = rng.Next(0, 10000000);
+                Console.WriteLine("Making elections with " + votersCount + " voters.");
+                int availableVotes = votersCount;
+                foreach (string candidate in elections.Keys.ToList())
+                {
+                    int votesForCandidate = rng.Next(0, availableVotes);
+                    elections[candidate] = votesForCandidate;
+                    availableVotes -= votesForCandidate;
+                }
+                EvaluateElections(elections);
+                Console.WriteLine(availableVotes + " voters (" + ((float)availableVotes / votersCount * 100) + " %) did not vote.");
+            }
+            else if (elections.Count == 1) Console.WriteLine("What is the point of making elections with only one candidate? We are not communists anymore :)");
+            else Console.WriteLine("You have no candidates in the elections.");
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> elections = new Dictionary<string, int>();
+
+            string userInput;
+
+            Console.WriteLine("Let's simulate elections!");
+
+            while (true)
+            {
+                Console.WriteLine("Choose your action:\n" +
+                    "\"Add\" - Add a new candidate\n" +
+                    "\"Remove\" - Remove a specific candidate\n" +
+                    "\"Erase\" - Remove every candidate\n" +
+                    "\"Vote\" - Set the number of votes for a specific candidate\n" +
+                    "\"Evaluate\" - Evaluate the current state of the elections\n" +
+                    "\"Random vote\" - Make random elections with existing candidates\n" +
+                    "\"Quit\" - Quit program");
+
+                userInput = Console.ReadLine().ToLower();
+                switch (userInput)
+                {
+                    case "add":
+                        Console.Write("Write the new candidate: ");
+                        AddCandidate(elections, Console.ReadLine());
+                        break;
+                    case "remove":
+                        Console.Write("Write the candidate who stepped away from the elections: ");
+                        RemoveCandidate(elections, Console.ReadLine());
+                        break;
+                    case "erase":
+                        Console.WriteLine("Erasing the whole elections.");
+                        EraseElections(elections);
+                        Console.WriteLine("Press any key to continue.");
+                        Console.ReadKey();
+                        break;
+                    case "vote":
+                        Console.Write("Write the candidate who's votes you want to set: ");
+                        string candidate = Console.ReadLine();
+                        while (!elections.ContainsKey(candidate))
+                        {
+                            Console.Write("No such candidate exists. Write an existing candidate: ");
+                            candidate = Console.ReadLine();
+                        }
+
+                        Console.Write("Write the number of votes you want to set: ");
+                        uint votes;
+                        while (!uint.TryParse(Console.ReadLine(), out votes))
+                        {
+                            Console.Write("Write a positive number, otherwise it does not make sense: ");
+                        }
+
+                        SetVotes(elections, candidate, votes);
+                        break;
+                    case "evaluate":
+                        EvaluateElections(elections);
+                        break;
+                    case "random vote":
+                        MakeRandomElections(elections);
+                        break;
+                    case "quit":
+                        return;
+                    default:
+                        Console.WriteLine("Unknown command. Try again.");
+                        break;
+                }
+
+                Console.Clear();
+                PrintCandidates(elections);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 2024-2025/Oktavy/9 - 8.11.2024/FavouriteFoods/FavouriteFoods/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavouriteFoods
+{
+    internal class Program
+    {
+        static void PrintList(List<string> list)
+        {
+            if (list.Count > 0)
+            {
+                Console.Write("Your list of favourite foods: ");
+                for (int i = 0; i < list.Count - 1; i++)
+                {
+                    Console.Write(list[i] + ", ");
+                }
+                Console.Write(list[list.Count - 1]);
+                Console.Write("\n");
+            }
+            else Console.WriteLine("Your list of favourite foods is empty :(");
+        }
+
+        static void AddFood(List<string> list, string food)
+        {
+            if (!list.Contains(food)) list.Add(food);
+            else Console.WriteLine("This food is already in your list.");
+        }
+
+        static void RemoveFood(List<string> list, string food)
+        {
+            if (list.Contains(food)) list.Remove(food);
+            else Console.WriteLine("This food is already not in your list.");
+        }
+
+        static void EraseList(List<string> list)
+        {
+            if (list.Count > 0) list.Clear();
+            else Console.WriteLine("Your list is already empty.");
+        }
+
+        static void Main(string[] args)
+        {
+            List<string> favouriteFoods = new List<string>();
+
+            string userInput;
+
+            Console.WriteLine("Let's create a list of your favourite foods!");
+
+            while (true)
+            {
+                Console.WriteLine("Choose your action:\n" +
+                    "\"Add\" - Add a new food to your list\n" +
+                    "\"Remove\" - Remove a specific food from your list\n" +
+                    "\"Erase\" - Remove every food from your list\n" +
+                    "\"Quit\" - Quit program");
+
+                userInput = Console.ReadLine().ToLower();
+                switch (userInput)
+                {
+                    case "add":
+                        Console.Write("Write your new favourite food: ");
+                        AddFood(favouriteFoods, Console.ReadLine().ToLower());
+                        break;
+                    case "remove":
+                        Console.Write("Write the food you no longer like: ");
+                        RemoveFood(favouriteFoods, Console.ReadLine().ToLower());
+                        break;
+                    case "erase":
+                        Console.WriteLine("Erasing the whole list.");
+                        EraseList(favouriteFoods);
+                        Console.WriteLine("Press any key to continue.");
+                        Console.ReadKey();
+                        break;
+                    case "quit":
+                        return;
+                    default:
+                        Console.WriteLine("Unknown command. Try again.");
+                        break;
+                }
+
+                Console.Clear();
+                PrintList(favouriteFoods);
+                Console.WriteLine();
+            }
+        }
+    }
+}
