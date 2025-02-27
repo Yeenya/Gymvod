ff574ae332c029c43b383820019f6c183264cda2
Jan Borecky <honza.borecky@seznam.cz>
2025-02-27 12:49:02 +0100

    Week 23

---
+++ 2024-2025/Oktavy/23 - 28.2.2025/GraphPlayground/GraphPlayground/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+/*
+ * Made by Jan Borecky for PRG seminar at Gymnazium Voderadska, year 2024-2025.
+ * Extended by students.
+ */
+
+namespace GraphPlayground
+{
+    internal class Program
+    {
+        public static void DFS(Graph graph, Node startNode, Node targetNode = null)
+        {
+            /*
+             * Zde naprogramuj prohledavani do hloubky.
+             */
+        }
+
+        public static void BFS(Graph graph, Node startNode, Node targetNode = null)
+        {
+            /*
+             * Zde naprogramuj prohledavani do sirky.
+             */
+        }
+
+        static void Main(string[] args)
+        {
+            //Create and print the graph
+            Graph graph = new Graph();
+            graph.PrintGraph();
+            graph.PrintGraphForVisualization();
+
+            //Call both algorithms with a random starting node
+            Random rng = new Random();
+            DFS(graph, graph.nodes[rng.Next(0, graph.nodes.Count)]);
+            BFS(graph, graph.nodes[rng.Next(0, graph.nodes.Count)]);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2024-2025/Septimy/23 - 28.2.2025/GraphPlayground/GraphPlayground/Graph.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GraphPlayground
+{
+    internal class Graph
+    {
+        public List<Node> nodes;
+
+        public Graph()
+        {
+            Random rng = new Random();
+
+            nodes = new List<Node>();
+            int nodeCount = rng.Next(5, 10);
+            for (int i = 0; i < nodeCount; i++)
+            {
+                nodes.Add(new Node(i));
+            }
+
+            //Randomly add neighbors to nodes with some limitations of the amount of neighbors for each node
+            foreach (Node node in nodes)
+            {
+                if (node.neighbors.Count >= 3) continue;
+                int neighborCount = rng.Next(1, 3);
+                for (int i = 0; i < neighborCount; i++)
+                {
+
+                    int attempts = 0;
+                    int newNeighborIndex = rng.Next(nodes.Count - 1);
+                    while ((newNeighborIndex == node.index || node.neighbors.Contains(nodes[newNeighborIndex])) && attempts < nodes.Count)
+                    {
+                        newNeighborIndex = rng.Next(nodes.Count - 1);
+                        attempts++;
+                    }
+                    if (attempts >= nodes.Count || nodes[newNeighborIndex].neighbors.Count >= 3) continue;
+                    node.neighbors.Add(nodes[newNeighborIndex]);
+                    if (!nodes[newNeighborIndex].neighbors.Contains(node))
+                    {
+                        nodes[newNeighborIndex].neighbors.Add(node);
+                    }
+                }
+            }
+        }
+
+        //Print the graph into console node by node
+        public void PrintGraph()
+        {
+            foreach (Node node in nodes)
+            {
+                Console.Write("Node " + node.index + " has neighbors: ");
+                foreach (Node neighbor in node.neighbors)
+                {
+                    Console.Write(neighbor.index + " ");
+                }
+                Console.Write("\n");
+            }
+        }
+
+        //Print edges to visualize the graph here: https://csacademy.com/app/graph_editor/
+        public void PrintGraphForVisualization()
+        {
+            foreach (Node node in nodes)
+            {
+                foreach (Node neighbor in node.neighbors)
+                {
+                    Console.WriteLine(node.index + " " + neighbor.index);
+                }
+            }
+        }
+    }
+}
